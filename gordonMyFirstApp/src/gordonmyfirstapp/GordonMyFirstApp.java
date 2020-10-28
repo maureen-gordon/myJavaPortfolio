25dfc8f6ffe63322d4612d723031f9f0420095fd
maureen-gordon <gordonmaureenm@gmail.com>
2020-10-28 04:49:48 -0500

    my first java app

---
+++ gordonMyFirstApp/src/gordonmyfirstapp/GordonMyFirstApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gordonmyfirstapp;
+
+/**
+ *
+ * @author Maureen Gordon
+ */
+public class GordonMyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Greeting g = new Greeting();
+    }
+    
+}
